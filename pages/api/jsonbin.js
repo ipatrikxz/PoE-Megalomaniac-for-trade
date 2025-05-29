a6f9ba9e5c57baddd693829b347053bf35fdc979
patriksasvarii@gmail.com <patriksasvarii@gmail.com>
2025-05-29 12:55:27 +0200

    🐛

--- pages/api/jsonbin.js
+++ pages/api/jsonbin.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const documentLink = process.env.JsonDocumentLink; // Fixed typo
+  const documentLink = process.env.JsonDocumentLink;
   const apiKey = process.env.JsonBinApiKey;
 
   try {
